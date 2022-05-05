65188f94993b85f958c8c982b8bfaa36f52d8bf4
xyz9999 <30603821+xyz9999@users.noreply.github.com>
2022-05-05 16:05:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:3000/');
+/*
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/',(req, res) => res.send('Hello World'));
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
+*/
