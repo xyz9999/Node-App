7559540d6b54967fe9fc09571eb473d779ce8e61
xyz9999 <30603821+xyz9999@users.noreply.github.com>
2022-05-05 16:16:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const hostname = 'http://ec2-52-45-170-173.compute-1.amazonaws.com/';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+/*
 var http = require('http');
 
 http.createServer(function (req, res) {
@@ -5,6 +19,7 @@ http.createServer(function (req, res) {
     res.end('Hello World\n');
 }).listen(3000, "0.0.0.0");
 console.log('Server running at http://0.0.0.0:3000/');
+/*
 /*
 const express = require('express');
 const app = express();
